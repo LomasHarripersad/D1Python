b0f2ec402bd8edbd7f43968cb0bba99fbff24151
LomasHarripersad <harripersadlomas@gmail.com>
2025-07-23 22:18:00 -0400

    Add files via upload

---
+++ task.py
@@ -0,0 +1,8 @@
+#version 1
+print("Welcome to the Band Name Generator.")
+city = input("what is the name of the city did you grow up in?\n")
+pet = input("what is the name of the pet?\n")
+
+print(city, pet + " could be your Band Name")
+
+input('press enter to continue')
